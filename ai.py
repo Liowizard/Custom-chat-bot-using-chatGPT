a6a5b32860f2ab710e62538b65121e4fc3b070ae
Lio_Wizard <ajeshrandam@gmail.com>
2023-05-19 19:32:40 +0530

    rit

--- ai.py
+++ ai.py
@@ -1,60 +1,43 @@
 import os 
 import json
 from urllib.parse import parse_qs
-os.environ['OPENAI_API_KEY'] = "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"
-
 from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext
 from langchain import OpenAI
 import sys
 import os
 from IPython.display import Markdown, display
 
+
+
+os.environ['OPENAI_API_KEY'] = "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"
+
+
+
 def construct_index(directory_path):
-    # set maximum input size
     max_input_size = 4096
-    # set number of output tokens
     num_outputs = 2000
-    # set maximum chunk overlap
     max_chunk_overlap = 20
-    # set chunk size limit
-    chunk_size_limit = 600 
-
-    # define prompt helper
+    chunk_size_limit = 600
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    # define LLM
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
- 
     documents = SimpleDirectoryReader(directory_path).load_data()
-    
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+    return index
 
-    #index.save_to_disk('index.json')
 
-    return index        
 
 index=construct_index("data")
-
 query_engine = index.as_query_engine()
-
-def ask_ai(name):
-    print(name)
-    #index = GPTVectorStoreIndex.load_from_disk('index.json')
-  #  while True: 
-    #query = input("What do you want to ask? ")
-    query_engine = index.as_query_engine()
-    response = query_engine.query(name)
-    print(response)
-
-
-# name=ask_ai("name")
 
 
 
 def parse_query_params(query_string):
     params = parse_qs(query_string)
     return {key: value[0] for key, value in params.items()}
+
+
+
 def hello_world(environ, start_response):
     query_string = environ.get('QUERY_STRING', '')
     query_params = parse_query_params(query_string)
@@ -71,6 +54,9 @@ def hello_world(environ, start_response):
     # else:
     #     response = {'message': 'Hello, World!'}
     return [json.dumps(response).encode('utf-8')]
+
+
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('', 8000, hello_world)
